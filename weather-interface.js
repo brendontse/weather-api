9754378b639687bc1fff91eaea7fa61c19f97048
brendon tse, kira roberts and molly stambaugh <brendon@>
2019-09-10 14:35:15 -0700

    add fahrenheit function

--- weather-interface.js
+++ weather-interface.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
     $('#location').val("");
 
     let request = new XMLHttpRequest();
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=[API-KEY-GOES-HERE]`;
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=4d503fd893c48b741f882ccca4231972`;
 
     request.onreadystatechange = function() {
       if (this.readyState === 4 && this.status === 200) {
@@ -16,9 +16,13 @@ $(document).ready(function() {
     request.open("GET", url, true);
     request.send();
 
+
    const getElements = function(response) {
       $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
-      $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
+      // $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
+      const fahrenheit = Math.round(((response.main.temp - 273.15) * 1.8) + 32)
+      $('.showTemp').text(`The temperature in Fahrenheit is ${fahrenheit} degrees.`);
+      console.log(fahrenheit);
     }
   });
 });
